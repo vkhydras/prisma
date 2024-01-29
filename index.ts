e4ce90a3d08c89cd0b6977e0a6fe34c2c4aa6970
vkhydras <victorkimaru8@gmail.com>
2024-01-29 23:02:38 +0300

    model

---
+++ index.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function main (){
+    //create user
+    // const user = await prisma.user.create({
+    //     data:{
+    //         name: "Vic Kim",
+    //         email:"vic@gmail.com"
+    //     }
+    // })
+
+    const users = await prisma.user.findMany({
+        include:{
+            articles: true
+        }
+    })
+    console.log(users)
+
+    //create article
+    // const article = await prisma.article.create({
+    //     data:{
+    //         title: 'Vics first Article',
+    //         body: 'This is Vic first article',
+    //         author: {
+    //             connect:{
+    //                 id: 1
+    //             }
+    //         }
+    //     }
+    // })
+
+    //associate article & user
+    // const user = await prisma.user.create({
+    //     data:{
+    //         name:"hydrastic Kim",
+    //         email:"hydrastic@gmail.com",
+    //         articles:{
+    //             create:{
+    //                 title:"hydrastics article",
+    //                 body:"This is hydrastic's first article"
+    //             }
+    //         }
+    //     }
+    // })
+
+
+    // const articles = await prisma.article.findMany()
+    // // console.log(user)
+    // console.log(articles)
+}
+
+main()
+    .then(async ()=>{
+        await prisma.$disconnect()
+    })
+    .catch(async(e)=>{
+        console.log(e)
+        await prisma.$disconnect
+        process.exit(1)
+    })
